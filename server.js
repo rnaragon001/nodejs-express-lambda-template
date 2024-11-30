a3ee7ef10e658268f6b8b40f3cb7241aca6a3139
Rod Aragon <rnaragon001@yahoo.com>
2024-12-01 00:20:39 +0800

    added serverless

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
@@ -9,3 +12,5 @@ app.get("/hello", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+module.exports.handler = serverless(app);
