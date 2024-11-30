9913a1db8e5eec822fa8a75296155d188166f42e
Rod Aragon <rnaragon001@yahoo.com>
2024-12-01 00:21:13 +0800

    removed listeners and ports

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
-const port = 3000;
 
 app.use(express.json());
 
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
 
 module.exports.handler = serverless(app);
